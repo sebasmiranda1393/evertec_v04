1d5f14c857dec45f34089b2a3b7f1cdb34d57cfc
sebasmiranda1393 <sebasmirandadc@gmail.com>
2020-06-29 17:42:37 -0500

    OSC-02 autenticaciòn con correo

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
